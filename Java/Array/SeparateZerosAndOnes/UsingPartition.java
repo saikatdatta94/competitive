23e9dae8c72046305a62db7240f72180ec70fc59
Saikat Datta <saikatdatta94@users.noreply.github.com>
2021-05-30 15:10:56 +0530

    Using two variables like partition algorithm

--- Java/Array/SeparateZerosAndOnes/UsingPartition.java
+++ Java/Array/SeparateZerosAndOnes/UsingPartition.java
@@ -1,1 +1,35 @@
+class Main{
 
+  public static void Main(String[] args){
+    int[] arr = {0,1,1,0,1,1,0};
+    int[] separated = usingPartition(arr,arr.length);
+    for(int i=0;i<separated.length;i++){
+      System.out.println(arr[i]);
+    }
+  
+  }
+
+//   Using two pointers
+  public static int[] usingPartition(int[] arr,int size){
+    int leftIndex=0,rightIndex=size-1;
+    
+    while(leftIndex < rightIndex){
+      while(leftIndex < rightIndex && arr[leftIndex]==0 ){
+        leftIndex++;
+      }
+      
+      while(leftIndex < rightIndex && arr[rightIndex]==1 ){
+        rightIndex--;
+      }
+      
+      if(leftIndex < rightIndex){
+        arr[leftIndex++] = 0;
+        arr[rightIndex--] = 1;
+      }
+      
+    }
+    
+    return arr;
+    
+  }
+}
